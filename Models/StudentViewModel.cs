f8d39031dc93003df7fcc52814abff3590966d6c
vijaya shanthi gajulagit config --global user.email <vijayashanthi.gajula@gmail.com>
2024-08-19 00:05:59 +1200

    StudentViewModel

--- Models/StudentViewModel.cs
+++ Models/StudentViewModel.cs
@@ -2,14 +2,27 @@
 {    
         public class StudentViewModel
     {
-        public IEnumerable<Student> Students { get; set; }
-        public IEnumerable<Course> Courses { get; set; }
-        public string FilterFN { get; set; }
-        public string FilterLN { get; set; }
+        //public IEnumerable<Student> Students { get; set; }
+        //public IEnumerable<Course> Courses { get; set; }
+        //public string FilterFN { get; set; }
+        //public string FilterLN { get; set; }
 
-        public string FilterPlace { get; set; }
-        public string FilterE { get; set; }
-        public IEnumerable<string> Places { get; set; } // Optional, for dropdown filter
-        public IEnumerable<string> Ethnicities { get; set; } // Optional, for dropdown filter 
+        //public string FilterPlace { get; set; }
+        //public string FilterE { get; set; }
+        //public IEnumerable<string> Places { get; set; } // Optional, for dropdown filter
+        //public IEnumerable<string> Ethnicities { get; set; } // Optional, for dropdown filter 
+
+
+
+
+
+        public List<Student> Students { get; set; } = new List<Student>();
+        public List<Course> Courses { get; set; } = new List<Course>();
+        public string FilterFN { get; set; } = string.Empty;
+        public string FilterLN { get; set; } = string.Empty;
+        public string FilterPlace { get; set; } = string.Empty;
+        public string FilterE { get; set; } = string.Empty;
+        public List<string> Places { get; set; } = new List<string>();
+        public List<string> Ethnicities { get; set; } = new List<string>();
     }
 }
